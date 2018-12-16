1f357c872f5a1e10a6c44a8c79b656957831232a
dcheng <dcheng@paloaltonetworks.com>
2018-12-16 00:02:36 -0800

    checkins

---
+++ 71. Simplify Path/Stack.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        Solution: Stack + Split
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        Inspired By: https://leetcode.com/problems/simplify-path/discuss/25691/9-lines-of-Python-code
+        :type path: str
+        :rtype: str
+        """
+        path = [p for p in path.split('/') if p != '.' and p != '']
+        stack = []
+        for p in path:
+            if p == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(p)
+        return '/' + '/'.join(stack)
+
+path = "/"
+print Solution().simplifyPath(path)
