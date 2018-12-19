4b13588cc50a5cc1955fd2d08d5927f1113bca49
dcheng <dcheng@paloaltonetworks.com>
2018-12-18 16:52:18 -0800

    new check-ins

--- 636. Exclusive Time of Functions/Stack.py
+++ 636. Exclusive Time of Functions/Stack.py
@@ -1,32 +1,31 @@
-# -*- coding: utf-8 -*-
 class Solution(object):
     def exclusiveTime(self, n, logs):
         """
+        Solution: Stack
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
+        Inspired By: https://leetcode.com/problems/exclusive-time-of-functions/discuss/105100/Python-Straightforward-with-Explanation
+        TP:
+        - To store "penalty time" in stack instead
         :type n: int
         :type logs: List[str]
         :rtype: List[int]
         """
         stack = []
         res = [0 for _ in range(n)]
-        i = 1
-        while i < len(logs):
-            curr_func, curr_type, curr_ts = logs[i].split(':')
-            prev_func, prev_type, prev_ts = logs[i-1].split(':')
-            interval = int(curr_ts) - int(prev_ts)
-            if curr_func == prev_func:
-                res[int(curr_func)] += interval+1
-            elif curr_type == prev_type == 'start':
-                stack.append([prev_func, interval])
-            elif curr_type == 'start' and prev_type == 'end':
-                stack.append([curr_func, interval])
-            elif curr_type == prev_type == 'end':
-                last_func, last_interval = stack.pop()
-                total_interval = last_interval + interval
-                res[int(last_func)] += total_interval
-            i += 1
+        for i in range(len(logs)):
+            func, type, time = logs[i].split(':')
+            func = int(func)
+            time = int(time)
+            if type == 'start':
+                stack.append(time)
+            else:
+                delta = time - stack.pop() + 1
+                res[func] = delta
+                stack = [t+delta for t in stack]
         return res
 
-n = 3
-logs = ["0:start:0","1:start:2","1:end:5","2:start:6","2:end:14","0:end:15"]
+n = 2
+logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
 
 print Solution().exclusiveTime(n, logs)
