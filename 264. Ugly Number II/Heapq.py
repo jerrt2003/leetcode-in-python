ee2e927f7339d031afb0d112362056b5cea16c81
dcheng <dcheng@paloaltonetworks.com>
2019-03-08 09:33:59 -0800

    2nd_scan/DP/#264

---
+++ 264. Ugly Number II/Heapq.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import heapq
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        Solution: heapq (priority queue)
+        Time Complexity: O(3*n)
+        Space Complexity: O(3*n)
+        Perf: Runtime: 896 ms, faster than 8.14% / Memory Usage: 10.9 MB, less than 35.21%
+        Inspired By: MySELF!!
+        :type n: int
+        :rtype: int
+        """
+        cand = [1]
+        for i in range(n-1):
+            k = heapq.heappop(cand)
+            if k*2 not in cand:
+                heapq.heappush(cand, k*2)
+            if k*3 not in cand:
+                heapq.heappush(cand, k*3)
+            if k*5 not in cand:
+                heapq.heappush(cand, k*5)
+        return heapq.heappop(cand)
+
+n = 10
+print Solution().nthUglyNumber(10)
