13683efbc86e9806b4c80553b54aaa445066766c
dcheng <dcheng@paloaltonetworks.com>
2019-03-25 09:37:39 -0700

    Google/#857

---
+++ 857. Minimum Cost to Hire K Workers/Heap.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import heapq
+class Solution(object):
+    def mincostToHireWorkers(self, quality, wage, K):
+        """
+        Solution: Heap
+        Time Complexity: O(nlog(n))
+        Space Complexity: O(n)
+        Perf: Runtime: 424 ms, faster than 31.63%  / Memory Usage: 14.1 MB, less than 12.00%
+        Inspired By:
+        - https://leetcode.com/problems/minimum-cost-to-hire-k-workers/discuss/237268/Python-heap-solution-with-clean-code-and-explanation
+        - https://leetcode.com/problems/minimum-cost-to-hire-k-workers/solution/
+        TP:
+        - Using wage[i]/quality[i] as sorting points
+        :type quality: List[int]
+        :type wage: List[int]
+        :type K: int
+        :rtype: float
+        """
+        m = len(quality)
+        wkr_ratio = []
+        for i in range(m):
+            wkr_ratio.append((float(wage[i])/quality[i], quality[i]))
+        wkr_ratio.sort()
+        h = []
+        min_cost = float('inf')
+        total_qua = 0
+        for ratio, qua in wkr_ratio:
+            total_qua += qua
+            heapq.heappush(h, -qua)
+            if len(h) == K:
+                min_cost = min(min_cost, total_qua*ratio)
+            elif len(h) > K:
+                total_qua += heapq.heappop(h)
+                min_cost = min(min_cost, total_qua*ratio)
+        return min_cost
